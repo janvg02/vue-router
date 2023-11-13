bf1ecd6f28c5d4b58f80e0b58c2c1bf2a1379969
janvg02 <jan.vangalen@student.pxl.be>
2023-11-13 10:25:51 +0100

    les einde

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '../scss/main.scss'
-
+import router from './router'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script >
+
+import {defineComponent} from "vue";
+import AboutComponent from "@/components/AboutComponent.vue";
+import CardComponent from "@/components/CardComponent.vue";
+
+export default defineComponent({
+  components: {CardComponent, AboutComponent}
+})
+
+</script>
+
+<template>
+  <AboutComponent/>
+
+  <section class="gallery">
+    <div class="cards-wrapper">
+      <CardComponent v-for="product in products"
+                     v-bind:key="product.id"
+                     v-bind:product="product"
+      />
+    </div>
+  </section>
+</template>
+
+<style >
+
+</style>
